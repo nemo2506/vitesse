3cf65b1c9f613b4f3eb0e4938730aad821a903e7
MARC NAVARRO <navamarc@gmail.com>
2025-07-14 11:28:26 +0200

    UPDATE DEPENDANCE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
+    alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.room)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -33,16 +36,53 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
+
+    //Configuration du plugin gradle de room
+    room {
+        schemaDirectory("$projectDir/schemas")
+    }
 }
 
 dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.hilt.android)
+    ksp(libs.room.compiler)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
+    implementation(libs.constraintlayout)
+    implementation(libs.activity.ktx)
+    implementation(libs.fragment.ktx)
+
+    coreLibraryDesugaring(libs.desugar)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.espresso)
+
+    //ajout des dépendances room
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+
+    //ajout de la dépendance Turbine
+    androidTestImplementation(libs.turbine)
+
+    //ajout de hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
+    implementation(libs.arch.core.common)
+    implementation(libs.arch.core.runtime)
+    testImplementation(libs.arch.core.testing)
+
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.turbine)
+    testImplementation(libs.kotlin.test)
 }
