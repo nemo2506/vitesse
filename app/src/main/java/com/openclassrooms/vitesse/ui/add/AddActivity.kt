9a2c1822ac9ef99009e0383acc3b2bf53c45331d
MARC NAVARRO <navamarc@gmail.com>
2025-07-21 02:53:15 +0200

    Upadate layout activity_add to color

--- app/src/main/java/com/openclassrooms/vitesse/ui/add/AddActivity.kt
+++ app/src/main/java/com/openclassrooms/vitesse/ui/add/AddActivity.kt
@@ -2,7 +2,6 @@ package com.openclassrooms.vitesse.ui.add
 
 import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,18 +12,6 @@ import com.openclassrooms.vitesse.databinding.ActivityAddBinding
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Calendar
 
-//class AddActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContentView(R.layout.activity_add)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-//    }
-//}
 @AndroidEntryPoint
 class AddActivity : AppCompatActivity() {
 
