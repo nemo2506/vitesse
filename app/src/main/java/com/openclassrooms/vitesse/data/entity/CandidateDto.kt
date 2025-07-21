16bdd9dda6152c9d8c62205e1527627d58afab9c
MARC NAVARRO <navamarc@gmail.com>
2025-07-21 19:09:48 +0200

    Update DTO with UNICITE to email attribut

--- app/src/main/java/com/openclassrooms/vitesse/data/entity/CandidateDto.kt
+++ app/src/main/java/com/openclassrooms/vitesse/data/entity/CandidateDto.kt
@@ -3,8 +3,12 @@ package com.openclassrooms.vitesse.data.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.Ignore
+import androidx.room.Index
 
-@Entity(tableName = "candidate")
+@Entity(
+    tableName = "candidate",
+    indices = [Index(value = ["email"], unique = true)]
+)
 data class CandidateDto(
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0,
