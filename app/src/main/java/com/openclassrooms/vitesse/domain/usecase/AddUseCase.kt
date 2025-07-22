86d62af36fefc4c30ee64183bd7217f196ac5ef1
MARC NAVARRO <navamarc@gmail.com>
2025-07-22 16:12:07 +0200

    Update to pattern DATE with Exception

--- app/src/main/java/com/openclassrooms/vitesse/domain/usecase/AddUseCase.kt
+++ app/src/main/java/com/openclassrooms/vitesse/domain/usecase/AddUseCase.kt
@@ -1,15 +1,26 @@
 package com.openclassrooms.vitesse.domain.usecase
 
+import android.util.Log
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 import javax.inject.Inject
 
 class AddUseCase @Inject constructor(){
 
-    fun getDateTime(birthDate: String): LocalDateTime {
-        val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
-        val localDate: LocalDate = LocalDate.parse(birthDate, formatter)
-        return localDate.atStartOfDay()
+    fun getDateTime(birthDate: String?): LocalDateTime? {
+        if (birthDate.isNullOrBlank()) {
+            Log.d("MARC", "getDateTime/isNullOrBlank: $birthDate")
+            return null
+        }
+        return try {
+            val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+            val localDate: LocalDate = LocalDate.parse(birthDate, formatter)
+            localDate.atStartOfDay()
+        } catch (e: DateTimeParseException) {
+            Log.d("MARC", "getDateTime/DateTimeParseException: $e")
+            null
+        }
     }
 }
