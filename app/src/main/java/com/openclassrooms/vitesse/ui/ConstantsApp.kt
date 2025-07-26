60b6ba0969ec43be56fa81e52690ca8e3403915f
MARC NAVARRO <navamarc@gmail.com>
2025-07-26 08:41:38 +0200

    Upgrade CONSTANT with detail id

--- app/src/main/java/com/openclassrooms/vitesse/ui/ConstantsApp.kt
+++ app/src/main/java/com/openclassrooms/vitesse/ui/ConstantsApp.kt
@@ -2,6 +2,7 @@ package com.openclassrooms.vitesse.ui
 
 sealed class ConstantsApp {
     companion object {
+        const val DETAIL_ID: String = "detailId"
         const val CANDIDATE_ID: String = "candidateId"
     }
 }
